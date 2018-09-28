832c034b07e96230552e56543952c609f71af2be
Anthony Logar <coledude919@gmail.com>
2018-09-28 19:17:16 -0400

    Movie plot search
    Added the ability to select only movies with a key word in it

--- bin/channel.py
+++ bin/channel.py
@@ -1,7 +1,7 @@
 import json
 data = []
 
-class Channel:
+class Channel(dict):
 
     ONE_MINUTE = 60000
     ONE_HOUR = 60 * ONE_MINUTE
@@ -9,12 +9,18 @@ class Channel:
 
     def __init__(self, path):
         with open(path) as f:
+            self.id = f.name
             self.data = json.load(f)
-
+        
         self.title = self.data["title"]
         self.description = self.data["description"]
         self.library = self.data["library"]
         self.type = self.data["type"]
+        self.plot = ""
+        try:
+            self.plot = self.data["plot"] 
+        except:
+            pass
 
 
     def getGeneralParams(self):
@@ -22,14 +28,28 @@ class Channel:
         
     def getChannel(self, path):
         return data
-    def generateSchedule(self, days=1):
+    def generateSchedule(self,startTime, days=1):
         import plex 
         import random
         import json
         server = plex.getServer()
+        # client = server.clients()[0]
+        
         searchParams = {}
         # searchParams = self.getGeneralParams()
         episodeList = []
+        movies = []
+
+        if self.type == "movie":
+            movies = plex.getMovies(server, **searchParams)
+            remMovies = []
+            if self.plot != '':
+                for m in movies:
+                    if m.summary.lower().find(self.plot.lower()) == -1:
+                        remMovies.append(m)
+            for m in remMovies:
+                movies.remove(m)            
+        
         duration = 0
         while duration < self.ONE_DAY * days :
             for d in self.data["general"]:
@@ -40,10 +60,16 @@ class Channel:
                 show = shows[random.randint(0,len(shows)-1)]
                 episodes = plex.getEpisodeBlock(server,self.library,show.title,self.data["general"]['show_block'])
                 for e in episodes:
-                    episodeList.append(Episode(e))
-                    duration += e.duration
-                    print (e.title)
-                
+                    ep = Episode(e)
+                    episodeList.append(ep)
+                    duration += ep.duration
+                    print (ep.title)
+            if self.type == "movie":           
+                movie = movies[random.randint(0,len(movies)-1)]
+                duration += movie.duration
+                print (movie.title)
+                episodeList.append(Movie(movie))
+
         
         return json.dumps(episodeList)
         # return ""
@@ -56,6 +82,7 @@ class Episode(dict):
         self.duration = episode.duration
         self.isWatched = episode.isWatched
         self.index = episode.index
+        # self.start_time = episode.start_time
         self.key = episode.key
         self.episode = episode.seasonEpisode
 
@@ -66,8 +93,35 @@ class Episode(dict):
             duration=self.duration,
             isWatched=self.isWatched,
             index=self.index,
+            # start_time=self.start_time,
             episode=self.episode,
             key=self.key)
+
+class Movie(dict):
+    def __init__(self, movie):
+        self.title = movie.title
+        # self.show_title = movie.grandparentTitle
+        self.duration = movie.duration
+        self.isWatched = movie.isWatched
+        self.year = movie.year
+        # self.start_time = movie.start_time
+        # self.index = movie.index
+        self.key = movie.key
+        # self.movie = movie.seasonmovie
+
+
+        dict.__init__(self, 
+            title=self.title,
+            # show_title=self.show_title,
+            duration=self.duration,
+            isWatched=self.isWatched,
+            # index=self.index,
+            year=self.year,
+            # start_time=self.start_time,
+            # episode=self.episode,
+            key=self.key)
+
+
     # def toJSON(self):
     #     return json.dumps(self, default=lambda o: o.__dict__, 
     #         sort_keys=True, indent=4)
